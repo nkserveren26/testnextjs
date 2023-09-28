2ddfc4f0280dbb55a7fd8eb328e4eaeb42d1cd97
nkserveren26 <norinoritoshi@outlook.jp>
2023-09-28 22:54:55 +0900

    add ssg page

---
+++ src/pages/ssg.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+type SSGProps = {}
+
+const SSG: NextPage<SSGProps> = () => {
+    return (
+        <>
+            <div>
+                <Head>
+                    <title>Static Site Generation</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main>
+                    <p>
+                        このページは静的サイト生成によってビルド時に生成されたページです
+                    </p>
+                </main>
+
+            </div>
+        </>
+    )
+
+}
+
+export default SSG
