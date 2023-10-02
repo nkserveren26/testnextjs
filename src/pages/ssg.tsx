224805654d50de949e6c58823aeffecd0a34db16
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-02 22:50:17 +0900

    add link

--- src/pages/ssg.tsx
+++ src/pages/ssg.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 type SSGProps = {
     message: string
@@ -21,6 +22,9 @@ const SSG: NextPage<SSGProps> = (props) => {
                     <p>
                         {message}
                     </p>
+                    <Link href="/ssr">
+                        <a>Go To SSR</a>
+                    </Link>
                 </main>
 
             </div>
