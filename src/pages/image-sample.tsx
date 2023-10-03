18c947668d9ac5d723caab3e122e73d04c3348d6
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-03 22:02:25 +0900

    add image-sample page

---
+++ src/pages/image-sample.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const ImageSample: NextPage<void> = () => {
+    return (
+        <div>
+            <h1>画像表示の比較</h1>
+        </div>
+    );
+}
+
+export default ImageSample;
