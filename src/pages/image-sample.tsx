79ef4d3f5d3e98e253f09891e0a8cdc17c0a343f
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-03 22:13:50 +0900

    add image to image page

--- src/pages/image-sample.tsx
+++ src/pages/image-sample.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next";
+import Image from "next/image";
+import CloudImage from "../../public/images/cloud.png";
 
 const ImageSample: NextPage<void> = () => {
     return (
         <div>
             <h1>画像表示の比較</h1>
+            <p>imgタグで表示した場合</p>
+            <img src="/images/cloud.png" />
+            <p>Imageコンポーネントで表示した場合</p>
+            <Image src={CloudImage} alt="image"/>
+            <p>Imageで表示した場合は事前に描画エリアが確保されます</p>
         </div>
     );
 }
