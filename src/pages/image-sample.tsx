962fc4bf647896ba60dbee4bf51d583a1ef35772
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-03 22:14:57 +0900

    add comment to image page

--- src/pages/image-sample.tsx
+++ src/pages/image-sample.tsx
@@ -9,6 +9,7 @@ const ImageSample: NextPage<void> = () => {
             <p>imgタグで表示した場合</p>
             <img src="/images/cloud.png" />
             <p>Imageコンポーネントで表示した場合</p>
+            {/* パスを指定する代わりにインポートした画像を指定可能 */}
             <Image src={CloudImage} alt="image"/>
             <p>Imageで表示した場合は事前に描画エリアが確保されます</p>
         </div>
