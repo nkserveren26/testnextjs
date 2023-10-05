9500899f372a25413c850fdf8a1163bdef7d3b40
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-05 23:22:17 +0900

    add api request to sayhello

--- src/pages/sayhello.tsx
+++ src/pages/sayhello.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Sayhello = () => {
     const [data, setData] = useState({name: ""});
+
+    //副作用はuseEffect内で処理するのがお作法
+    //外部API実行は副作用に該当するのでuseEffect内に記載
+    useEffect(() => {
+        fetch("api/hello")
+        .then((res) => res.json())
+        .then((profile) => {
+            setData(profile);
+        });
+    });
 
     return <div>hello {data.name}</div>
     
