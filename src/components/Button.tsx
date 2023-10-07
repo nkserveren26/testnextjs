bfaad68a365121055c6828127fdb0da6dafb47bb
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-07 11:34:58 +0900

    correct button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,19 @@
 type ButtonProps = {
-
+    label: string,
+    text: string,
+    disabled: boolean,
+    onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export const Button = (props: ButtonProps) => {}
+export const Button = (props: ButtonProps) => {
+    const {label, text, disabled, onClick } = props;
+    return (
+        <div>
+            <span>{label}</span>
+            <button disabled={disabled} onClick={onClick}>
+                {text}
+            </button>
+        </div>
+    );
+
+}
