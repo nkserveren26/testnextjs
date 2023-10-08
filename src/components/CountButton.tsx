2f83a73b57c45a55a15b0b58f3be05616ae4fd03
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-08 23:27:57 +0900

    add usePopup in counterbutton

--- src/components/CountButton.tsx
+++ src/components/CountButton.tsx
@@ -1,3 +1,14 @@
+import { useCallback } from "react"
+
+const UsePopup = () => {
+
+    const cb = useCallback((text: string) => {
+        prompt(text);
+    }, []);
+
+    return cb;
+}
+
 type CounterButtonProps = {
     label: string,
     maximum: number
