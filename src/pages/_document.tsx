c72579aa149c6479e6fda4f76ac73d9237b76ad0
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-11 23:05:05 +0900

    add document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => {
+        return (
+          originalRenderPage({
+            enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          })
+        )
+      }
+
+      //初期値を流用
+      const initialProps = await Document.getInitialProps(ctx);
+
+      //initialPropsに加えてstyleを追加して返す
+      return {
+        ...initialProps,
+        styles: [
+          //もともとのstyle
+          initialProps.styles,
+          sheet.getStyleElement()
+        ]
+      }
+    } finally {
+      sheet.seal();
+    }
+  }
 }
