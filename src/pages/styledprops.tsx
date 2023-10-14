d4e590ddc2bb9b804f7b85b7b58315f6c44970a6
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-14 23:27:55 +0900

    add another button in styledprops

--- src/pages/styledprops.tsx
+++ src/pages/styledprops.tsx
@@ -1,11 +1,19 @@
 import { NextPage } from "next";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 type ButtonProps = {
     backGroundColor: string,
     color: string
 }
 
+//styled-componentは、cssの定義を共通化して各コンポーネントで再利用できる
+const redBox = css`
+    padding: 0.25em 1em;
+    border: 3px solid #ff0000;
+    border-radius: 10px;
+`;
+
+//styled-componentで定義したコンポーネントの先頭文字は大文字にする。小文字だとエラーになる
 const Button = styled.button<ButtonProps>`
     /* color, background, borderはpropsの値を使用 */
     color: ${(props) => props.color};
@@ -13,7 +21,12 @@ const Button = styled.button<ButtonProps>`
     border: 2px solid ${(props) => props.color};
 
     font-size: 2em;
-`
+`;
+
+const AnotherButton = styled.button`
+    color: "blue";
+    ${redBox}
+`;
 
 const Styledprops: NextPage = () => {
     return (
@@ -21,6 +34,10 @@ const Styledprops: NextPage = () => {
             <Button color="#FF0000" backGroundColor="transparent">
                 Hello
             </Button>
+            <p>another button</p>
+            <AnotherButton>
+                GoodBye
+            </AnotherButton>
         </div>
     );
 }
