18acde4a20d69f420a2b475b9a4fdd602ee4e7bf
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-14 23:10:01 +0900

    correct styledprops

--- src/pages/styledprops.tsx
+++ src/pages/styledprops.tsx
@@ -2,17 +2,20 @@ import { NextPage } from "next";
 import styled from "styled-components";
 
 type ButtonProps = {
+    backGroundColor: string,
     color: string
 }
 
 const Button = styled.button<ButtonProps>`
     color: ${(props) => props.color};
+    background: ${(props) => props.backGroundColor};
+    border: 2px solid ${(props) => props.color};
 `
 
 const Styledprops: NextPage = () => {
     return (
         <div>
-            <Button color="#FF0000">
+            <Button color="#FF0000" backGroundColor="transparent">
                 Hello
             </Button>
         </div>
