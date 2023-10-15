eb68e15875d9e975ff40724bce235fe3d894f607
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-15 23:35:20 +0900

    add BaseLinkProps

--- src/pages/stylesample.tsx
+++ src/pages/stylesample.tsx
@@ -1,4 +1,14 @@
 import { NextPage } from "next";
+import { LinkProps } from "next/link";
+
+type BaseLinkProps = React.PropsWithChildren<LinkProps> & {
+    className?: string,
+    children: React.ReactNode
+}
+
+const BaseLink = () => {
+
+}
 
 const StyleSample: NextPage = () => {
     return (
