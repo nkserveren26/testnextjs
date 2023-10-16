66be44965621eb7a89cc74311f30b5f79acdb6d1
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-16 23:46:16 +0900

    add ThemeProvider in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    //Themeを使う際はThemeProviderを使用し、themeプロパティに値を渡す
+    <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+    </ThemeProvider>
+  ); 
 }
