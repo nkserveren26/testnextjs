3ceea2bbb1ecd90842624a65ea02fa7ed9978d29
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-16 23:50:04 +0900

    correct tyemesample

---
+++ src/pages/themesample.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import { styled } from "styled-components";
+
+const Text = styled.span`
+    color: ${(props) => props.theme.colors.red}
+`;
+
+const ThemeSample: NextPage = () => {
+    return (
+        <div>
+            <Text>Themeから参照した色を使用</Text>
+        </div>
+    );
+}
+
+export default ThemeSample;
