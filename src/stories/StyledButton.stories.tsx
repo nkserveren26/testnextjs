eec75b0855d26b70a2f8d73df0996a909d3c8e15
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-21 23:59:48 +0900

    commit

--- src/stories/StyledButton.stories.tsx
+++ src/stories/StyledButton.stories.tsx
@@ -1,1 +1,9 @@
-export default {}
+import { StyledButton } from "@/components/StyledButton";
+import { ComponentMeta } from "@storybook/react";
+
+export default {
+    // グループ名
+    title: "StyledButton",
+    // 使用するコンポーネント
+    component: StyledButton,
+} as ComponentMeta<typeof StyledButton>
