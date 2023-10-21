b7816c3be6a098219b0ef4e8a4d8133e9f8bfea9
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-21 23:52:37 +0900

    commit

--- src/components/StyledButton/index.tsx
+++ src/components/StyledButton/index.tsx
@@ -1,3 +1,5 @@
+import { css, styled } from "styled-components"
+
 const variants = {
     primary: {
         color: "#fffff",
@@ -9,4 +11,20 @@ const variants = {
         backgroundColor: "#SAB203",
         border: "none"
     }
+} as const
+
+type StyledButtonProps = {
+    variant: keyof typeof variants
 }
+
+export const StyledButton = styled.button<StyledButtonProps>`
+    ${({ variant }) => {
+        const style = variants[variant]
+        return css`
+            color: ${style.color};
+            background-color: ${style.backgroundColor};
+            border: ${style.border};
+        `;
+    }}
+`;
+
