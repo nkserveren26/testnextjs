c1ef9e775a7c8b95b76161b08bd62034ea53f1ba
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-21 23:44:38 +0900

    add styledbutton layout

---
+++ src/components/StyledButton/index.tsx
@@ -0,0 +1,12 @@
+const variants = {
+    primary: {
+        color: "#fffff",
+        backgroundColor: "#103461",
+        border: "none"
+    },
+    success: {
+        color: "#fffff",
+        backgroundColor: "#SAB203",
+        border: "none"
+    }
+}
