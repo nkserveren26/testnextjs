96a046180b6f1466353cec3897e49bd2e5b97270
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-22 23:33:24 +0900

    correct styledbutton

--- src/stories/StyledButton.stories.tsx
+++ src/stories/StyledButton.stories.tsx
@@ -1,17 +1,32 @@
 import { Meta } from "@storybook/react";
 import { StyledButton } from "../components/StyledButton";
+import { action } from "@storybook/addon-actions";
+import { useState } from "react";
 
 export default {
     // グループ名
     title: "StyledButton",
     // 使用するコンポーネント
     component: StyledButton,
+    //onClickが呼ばれたときにclickedというアクションを出力する
+    //argTypes: { onClick: {action: "clicked"}},
 } as Meta<typeof StyledButton>;
 
+//任意のデータをActionタブに表示したい場合はactionsで定義する
+const incrementAction = action("increment");
+
+//変数名がStory上に表示される
 export const Primary = (props) => {
+    const [count, setCount] = useState(0);
+    const onClick = (e: React.MouseEvent) => {
+        //引数にActionタブで表示したい値を渡す
+        incrementAction(e, count);
+        setCount((c: number) => c + 1);
+    }
+
     return (
-        <StyledButton {...props} variant="primary">
-            Primary
+        <StyledButton {...props} variant="primary" onClick={onClick}>
+            Count: {count}
         </StyledButton>
     )
 };
