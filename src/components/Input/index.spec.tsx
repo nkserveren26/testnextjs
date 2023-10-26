5f849990a001191499e567ae4f0ded68d5f84b82
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-26 23:47:21 +0900

    add afterEach in index.spec.tsx

--- src/components/Input/index.spec.tsx
+++ src/components/Input/index.spec.tsx
@@ -1,11 +1,20 @@
 import {render, RenderResult} from "@testing-library/react";
-import { beforeEach, describe } from "node:test";
+import { afterEach, beforeEach, describe } from "node:test";
 import { Input } from "./index";
 
 describe("Input", () => {
     let renderResult: RenderResult;
+
+    //テストケース前にコンポーネントを描画し、renderResultにセット
     beforeEach(() => {
         renderResult = render(<Input id="username" label="Username" />);
     });
+
+    //テストケース実行後に描画していたコンポーネントを開放する
+    afterEach(() => {
+        renderResult.unmount();
+    });
+
+
 
 });
